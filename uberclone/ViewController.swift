514ea9bfff77488ae104d6b92909042380d7ac47
Carlos Torres <>
2018-12-10 16:58:39 -0800

    Add new view for choosing the user destination
    The constraints and the stackview needs to be fixed ASAP

---
+++ uberclone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uberclone
+//
+//  Created by Carlos Torres Sanchez on 12/6/18.
+//  Copyright © 2018 Carlos Torres Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
